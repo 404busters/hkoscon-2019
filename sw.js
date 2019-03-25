568a50b7a4a156f07abb697ef8a789890d90b271
CI Auto Build <tony@tonyhhyip.me>
2019-03-25 03:16:54 +0000

    CI: Build of 1df33d0bd28830b0d8067b489d811f70da593bb8

--- sw.js
+++ sw.js
@@ -1,141 +1,141 @@
-importScripts('/hkoscon-2019/workbox.4c4f5ca6.js')
+importScripts('/hkoscon-2019/workbox.30cfab93.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/hkoscon-2019/029940c550cc8f761e0f.js",
-    "revision": "8070af57da3f44bf144a4a6b1c6f72ca"
-  },
-  {
-    "url": "/hkoscon-2019/136286a94b0d8f13fe33.js",
-    "revision": "b1cebe65865877dbc9b9398e96fb90c9"
-  },
-  {
-    "url": "/hkoscon-2019/165adb37cb4e545bab85.css",
-    "revision": "13de707a1eae6b5a8b8abee3e3fcd1a4"
-  },
-  {
-    "url": "/hkoscon-2019/1ed4011073ade26e75dc.js",
-    "revision": "70373a0545d0ff8b4a1dd053ca17a83a"
-  },
-  {
-    "url": "/hkoscon-2019/2b22477f4c78afa26fb1.css",
-    "revision": "10d5d691aa0b3533093b57c41687a438"
-  },
-  {
-    "url": "/hkoscon-2019/38ef93572b9fcb551354.js",
-    "revision": "4be3c5ca26c5073e4f036d18bedf4865"
-  },
-  {
-    "url": "/hkoscon-2019/4506b8a34034379efd79.css",
-    "revision": "f0ff665a8c84f76dbfeb5edc8ac6f809"
-  },
-  {
-    "url": "/hkoscon-2019/50e5ab0e7310fa8a3d3c.js",
-    "revision": "b2cbc32a47ab935e44b26f0094f9a356"
-  },
-  {
-    "url": "/hkoscon-2019/518dcc1f802ccc7e410c.js",
-    "revision": "da66cb39315d41e3ad21d013df55e5d0"
-  },
-  {
-    "url": "/hkoscon-2019/5c3630433011d62c98c6.js",
-    "revision": "988dbc08dfc650fefd1c33e9b8f07d5d"
-  },
-  {
-    "url": "/hkoscon-2019/5f9a9b8c1e82f06c9050.js",
-    "revision": "063a99fb2414e39232029af5e577082e"
-  },
-  {
-    "url": "/hkoscon-2019/6a25d7659e1a63369839.css",
+    "url": "/hkoscon-2019/096016304cbe014886d7.css",
     "revision": "0c02e47203b1290ad5f5abb4f970155e"
   },
   {
-    "url": "/hkoscon-2019/7300930f5c5e009d91d3.js",
-    "revision": "fa4ec2a5300719a0fe0256dd791bca27"
+    "url": "/hkoscon-2019/10f6fd242ebe738acb05.js",
+    "revision": "6954fe074697c8e49234ec11c8ad1406"
   },
   {
-    "url": "/hkoscon-2019/785c1584cbfb2011d1b6.js",
-    "revision": "0ad6287a8e1007bdb5ea5aa443bd694a"
+    "url": "/hkoscon-2019/2506df3260a40c791622.css",
+    "revision": "13de707a1eae6b5a8b8abee3e3fcd1a4"
   },
   {
-    "url": "/hkoscon-2019/8b9166ad1744ce64c29d.css",
-    "revision": "9fdc55af5d148081693de80e238a9c5e"
+    "url": "/hkoscon-2019/294ad8b9a6ad3712ae2c.js",
+    "revision": "125383538bfa308f193ae5d97e2226a4"
   },
   {
-    "url": "/hkoscon-2019/8c7f6f191b2632a0db52.js",
-    "revision": "0cf744e028b24569ab9ffb1225233e3a"
+    "url": "/hkoscon-2019/296533b7d478cfcd1e66.js",
+    "revision": "c14fa024a13a53165174c76ba48ea69b"
   },
   {
-    "url": "/hkoscon-2019/921d290d74d2ee405b68.css",
-    "revision": "43d5405ab0f4bc41a7452dbc00d25e15"
+    "url": "/hkoscon-2019/2a614a5e1b8e10a14e2b.js",
+    "revision": "ef9f6d86d1da80b97bd6dbd9763ca33c"
   },
   {
-    "url": "/hkoscon-2019/9407c00abe1d47296fb8.css",
+    "url": "/hkoscon-2019/310bdc477ad1e82df22a.css",
+    "revision": "f0ff665a8c84f76dbfeb5edc8ac6f809"
+  },
+  {
+    "url": "/hkoscon-2019/500b2bf34a0fe9a2c0ce.css",
+    "revision": "10d5d691aa0b3533093b57c41687a438"
+  },
+  {
+    "url": "/hkoscon-2019/60afb414c2606448f71c.js",
+    "revision": "5f27210e724a27a3a894c2ef9912acfa"
+  },
+  {
+    "url": "/hkoscon-2019/700434804c1f28373825.js",
+    "revision": "5a313e148305d09ae30fae35e291ba4c"
+  },
+  {
+    "url": "/hkoscon-2019/739a41bd04ecdee99dde.js",
+    "revision": "531147bd0ec7d53013ce12d155d2cd46"
+  },
+  {
+    "url": "/hkoscon-2019/7af1a37853ca7c35ad10.css",
     "revision": "d32ffbbec375bf9eaf56aceb9fd1a38c"
   },
   {
-    "url": "/hkoscon-2019/97615451905755c15a14.js",
-    "revision": "4d07c7efa4be20bd06e6d3745ecb9271"
+    "url": "/hkoscon-2019/7d47daae7f6985c9f520.js",
+    "revision": "3f1ffcca9ec0558bb98381159d820814"
   },
   {
-    "url": "/hkoscon-2019/9d36c7c092a78f74f295.js",
-    "revision": "1732e164ed0b750505fa858375867dbe"
-  },
-  {
-    "url": "/hkoscon-2019/a7b1c3444ed6895a41f3.js",
-    "revision": "4c6cda912e2d84960e50305fcfb6951b"
-  },
-  {
-    "url": "/hkoscon-2019/b021431ce514776eec39.css",
-    "revision": "f52be865dfa469658249d4550bab557b"
-  },
-  {
-    "url": "/hkoscon-2019/b10df927e42e73c8f81a.js",
-    "revision": "f0a77c1795e8e3e2d4993e1f76bcd546"
-  },
-  {
-    "url": "/hkoscon-2019/b2a1e20abb2795630d55.js",
-    "revision": "b0c1d50bcf32233d6bd1dc1371dc7cd6"
-  },
-  {
-    "url": "/hkoscon-2019/c934a542dfca2fe79df8.css",
-    "revision": "8c2ce15200a6b500719375696c468d52"
-  },
-  {
-    "url": "/hkoscon-2019/ca166ea896ebeab926d5.css",
-    "revision": "a4dcdb2f6778b7814cf929c64f268456"
-  },
-  {
-    "url": "/hkoscon-2019/cbb608d5debab25086f3.css",
-    "revision": "d0250849b7de8a9d4788a412693f849b"
-  },
-  {
-    "url": "/hkoscon-2019/cbbe5a998fe8cbc9a878.css",
-    "revision": "df4b7c436dea4b56cfff94a89031318a"
-  },
-  {
-    "url": "/hkoscon-2019/d0db2eb6b6991ecd8ebe.js",
-    "revision": "42805106a87a5b925b529413981089de"
-  },
-  {
-    "url": "/hkoscon-2019/d1b69c03a26420408e40.css",
+    "url": "/hkoscon-2019/7f58d10c6743a8a2bb6c.css",
     "revision": "1c284c5762aaf870b72d55f0c9c89822"
   },
   {
-    "url": "/hkoscon-2019/dc14d1eac4ad3e8baf35.js",
-    "revision": "9a3d4b1b613b7c496487a41f56ded873"
+    "url": "/hkoscon-2019/8c4634da73be5f5d2890.js",
+    "revision": "c994faf92be3ac6a64dcf3d321e33fc9"
   },
   {
-    "url": "/hkoscon-2019/f7f8281a5818c2b77921.js",
-    "revision": "f30888a8f9eace5dbc1bcaf021fa51ce"
+    "url": "/hkoscon-2019/954aaf5126a0d52c1a97.js",
+    "revision": "8f169d1314a3a517380f6d235da3bb24"
   },
   {
-    "url": "/hkoscon-2019/fd9655fb46e867b61956.js",
-    "revision": "1d63de33d92b057b16c1ada4eca62f45"
+    "url": "/hkoscon-2019/afbc3e4397929e5d6eb7.js",
+    "revision": "8bfc64c72987faaa10514a68a34480f8"
   },
   {
-    "url": "/hkoscon-2019/ff03764063841ac41946.js",
-    "revision": "7a747bb174e89925c76662be9d1838ad"
+    "url": "/hkoscon-2019/b446fe26bc5653933799.js",
+    "revision": "e7536627df2720d6622546d1f4dc595d"
+  },
+  {
+    "url": "/hkoscon-2019/b5e4854a8c7eb354cc36.js",
+    "revision": "64e183ff7160f13d2e025dc2deb2c803"
+  },
+  {
+    "url": "/hkoscon-2019/c08eec3748036dc29f58.css",
+    "revision": "43d5405ab0f4bc41a7452dbc00d25e15"
+  },
+  {
+    "url": "/hkoscon-2019/c758dd9f5e5fbdbcb6a0.js",
+    "revision": "41c476791872b15d99e73b7c1ef0ca45"
+  },
+  {
+    "url": "/hkoscon-2019/cb6334ead159038654dd.css",
+    "revision": "f52be865dfa469658249d4550bab557b"
+  },
+  {
+    "url": "/hkoscon-2019/ccad310ae3cc955add64.css",
+    "revision": "8c2ce15200a6b500719375696c468d52"
+  },
+  {
+    "url": "/hkoscon-2019/ccddb045c8e483a84ef4.js",
+    "revision": "ecdcdfe726dd5eb41ef6cbe86e49f81d"
+  },
+  {
+    "url": "/hkoscon-2019/d1d212570e26027c294f.css",
+    "revision": "df4b7c436dea4b56cfff94a89031318a"
+  },
+  {
+    "url": "/hkoscon-2019/d55fa3de7e1d99a939a7.js",
+    "revision": "f28f6b7c728702416382153676346698"
+  },
+  {
+    "url": "/hkoscon-2019/d8ba175e6fd1ba5dd720.js",
+    "revision": "16381a1e47c2c2ce627c490b332fae77"
+  },
+  {
+    "url": "/hkoscon-2019/da2074b38c75d8d1a176.css",
+    "revision": "d0250849b7de8a9d4788a412693f849b"
+  },
+  {
+    "url": "/hkoscon-2019/dac6590df692d2a2987c.js",
+    "revision": "d3d205af94795357de651be21e8e4fd8"
+  },
+  {
+    "url": "/hkoscon-2019/e2fc9ba6dee8c6598de2.js",
+    "revision": "c5b9e0b2123a234491422963561402fb"
+  },
+  {
+    "url": "/hkoscon-2019/e801add50cbbda846668.js",
+    "revision": "2c2e39cb2684027e548e0b6fe5dd464e"
+  },
+  {
+    "url": "/hkoscon-2019/eddcf96035150219b6a4.js",
+    "revision": "3af31c025333869a7b5aa27f4f4b47f6"
+  },
+  {
+    "url": "/hkoscon-2019/f0c8915a03fb5173592b.css",
+    "revision": "9fdc55af5d148081693de80e238a9c5e"
+  },
+  {
+    "url": "/hkoscon-2019/f738d9744ebeff326421.css",
+    "revision": "a4dcdb2f6778b7814cf929c64f268456"
   }
 ], {
   "cacheId": "2019",
